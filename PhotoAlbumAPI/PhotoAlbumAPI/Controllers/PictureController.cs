f88d01252664697b838cbefd2e0a7f833e7da68b
tamar96033 <tamar96033@gmail.com>
2025-03-11 18:06:22 +0200

    add dockerfile

--- PhotoAlbumAPI/PhotoAlbumAPI/Controllers/PictureController.cs
+++ PhotoAlbumAPI/PhotoAlbumAPI/Controllers/PictureController.cs
@@ -17,26 +17,5 @@ namespace PhotoAlbum.API.Controllers
         }
 
 
-
-        [HttpPost]
-        public async Task<ActionResult<Picture>> CreatePicture(Picture picture)
-        {
-            if (picture == null)
-            {
-                return BadRequest("Invalid data.");
-            }
-
-            // Ensure the PictureTags are initialized
-            if (picture.PictureTags == null)
-            {
-                picture.PictureTags = new List<PictureTag>();
-            }
-
-            // Add the Picture to the context
-            _context.Pictures.Add(picture);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(CreatePicture), new { id = picture.Id }, picture);
-        }
     }
 }
